c6e77a7d94dd39a31baeb94a80c96f6557cd2f94
Oliver Wieland <oliver.wieland@unitybox.de>
2021-04-21 00:14:43 +0200

    Fix template decl acc. to stackoverflow

--- src/listener.h
+++ src/listener.h
@@ -7,70 +7,66 @@
 
        http://www.apache.org/licenses/LICENSE-2.0
 
-	   Unless required by applicable law or agreed to in writing, software
-	   distributed under the License is distributed on an "AS IS" BASIS,
-	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	   See the License for the specific language governing permissions and
-	   limitations under the License.
+           Unless required by applicable law or agreed to in writing, software
+           distributed under the License is distributed on an "AS IS" BASIS,
+           WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+   implied. See the License for the specific language governing permissions and
+           limitations under the License.
 */
 
 #ifndef CBOR_CPP_LISTENER_H
 #define CBOR_CPP_LISTENER_H
 
-#include <string>
 #include <iostream>
+#include <string>
 
 namespace cbor {
-    struct listener {
-    
-        virtual void on_integer(int value) const noexcept {}
+struct listener {
 
-        virtual void on_float32(float value) const noexcept {}
-        virtual void on_double(double value) const noexcept {}
+  virtual void on_integer(int value) const noexcept {}
 
-        virtual void on_bytes(unsigned char *data, int size) const noexcept {}
+  virtual void on_float32(float value) const noexcept {}
+  virtual void on_double(double value) const noexcept {}
 
-        virtual void on_string(std::string &str) const noexcept {}
+  virtual void on_bytes(unsigned char *data, int size) const noexcept {}
 
-        virtual void on_array(int size) const noexcept {}
+  virtual void on_string(std::string &str) const noexcept {}
 
-        virtual void on_map(int size) const noexcept {}
+  virtual void on_array(int size) const noexcept {}
 
-        virtual void on_tag(unsigned int tag) const noexcept {}
+  virtual void on_map(int size) const noexcept {}
 
-        virtual void on_special(unsigned int code) const noexcept {}
-        
-        virtual void on_bool(bool) const noexcept {}
-        
-        virtual void on_null() const noexcept {}
-        
-        virtual void on_undefined() const noexcept {}
+  virtual void on_tag(unsigned int tag) const noexcept {}
 
-        virtual void on_error(const char *error) const noexcept {}
+  virtual void on_special(unsigned int code) const noexcept {}
 
-        virtual void on_extra_integer(unsigned long long value, int sign) const noexcept {
-        }
+  virtual void on_bool(bool) const noexcept {}
 
-        virtual void on_extra_tag(unsigned long long tag) const noexcept {
-        }
+  virtual void on_null() const noexcept {}
 
-        virtual void on_extra_special(unsigned long long tag) const noexcept {
-        }
+  virtual void on_undefined() const noexcept {}
 
-    protected:
-        template<typename T>
-        void p(const std::string & tag, const T& value) const noexcept {
-            std::cout << "[" << tag << sizeof(T) << " = " << value << "]\n";
-        }
+  virtual void on_error(const char *error) const noexcept {}
 
-        template<>
-        void p(const std::string & tag, const std::string& value) const noexcept {
-            std::cout << "[" << tag << value.length() << " = '" << value << "']\n";
-        } 
-    };
-}
+  virtual void on_extra_integer(unsigned long long value,
+                                int sign) const noexcept {}
 
+  virtual void on_extra_tag(unsigned long long tag) const noexcept {}
 
+  virtual void on_extra_special(unsigned long long tag) const noexcept {}
 
+protected:
+  template <typename T>
+  void p(const std::string &tag, const T &value) const noexcept {
+    std::cout << "[" << tag << sizeof(T) << " = " << value << "]\n";
+  }
+
+  template <>
+  void p<std::string>(const std::string &tag, const std::string &value) const noexcept {
+    std::cout << "[" << tag << value.length() << " = '" << value << "']\n";
+  }
+};
+
+} // namespace cbor
 
 #endif // CBOR_CPP_LISTENER_H
